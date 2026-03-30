7aa87e110c9c07d5bad4e00af0e0e62022c0c0eb
Veytrinanjunita <bulanveytrinanjunita@gmail.com>
2026-03-30 21:27:30 +0700

    commit14

--- looping.cpp
+++ looping.cpp
@@ -148,3 +148,11 @@ void pilihanJurusan()
         statusKuota = "Error: Kode Tidak Valid";
     }
 }
+
+void output()
+{
+    cout << "\n=== HASIL SELEKSI ===" << endl;
+    cout << "Jurusan Pilihan : " << namaJurusan << endl;
+    cout << "Status Kuota    : " << statusKuota << endl;
+    cout << "----------------------------------" << endl;
+}
