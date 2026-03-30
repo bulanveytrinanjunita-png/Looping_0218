172819f5629fc221be75947cb1173b58d9d20e8b
Veytrinanjunita <bulanveytrinanjunita@gmail.com>
2026-03-30 20:48:33 +0700

    commit4

--- looping.cpp
+++ looping.cpp
@@ -39,3 +39,26 @@ int main()
     cout << "Bilangan acak do-while yang terakhir =" << x << endl;
     cout << endl;
 }
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float nilB, nilM, rerata;
+    string status;
+
+    cout << "Masukan nilai Matematika = ";
+    cin >> nilM;
+    cout << "Masukan nilai Bahasa Inggris = ";
+    cin >> nilB;
+
+    rerata = (nilB + nilM) / 2;
+
+    if (!(rerata < 60))
+        status = "Lulus";
+    else
+        status = "Tidakl lulus";
+
+    cout << "Status kelulusan = " << status << " ,dengan nilai rata-rata = " << rerata << endl;
+}
